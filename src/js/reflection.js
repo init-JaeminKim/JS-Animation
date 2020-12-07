2e530fdd6898fd138d2a33cf365ebde39bb5bdf8
김재민 <jkim37@g.emporia.edu>
2020-12-07 10:34:56 -0600

    feflection

---
+++ src/js/reflection.js
@@ -0,0 +1,72 @@
+var canvas = document.getElementById('reflection');
+var rflx_ctx = canvas.getContext('2d');
+var canvasInterval = requestAnimationFrame(rflx_canvasAnimation);
+var rflx_circleX = canvas.width / 2
+var rflx_circleY = (Math.random() * 150) + 10;
+var rflx_dX = 2;
+var rflx_dY = -2;
+var rflx_gravity = 0.1;
+var rflx_friction = 0.9;
+var rflx_energy = 0.75;
+var rflx_radius = 10;
+
+
+function rflx_canvasAnimation() {
+
+    rflx_drawCircle(rflx_circleX, rflx_circleY, rflx_radius);
+    canvasInterval = requestAnimationFrame(rflx_canvasAnimation);
+}
+
+function rflx_drawCircle(x, y, s) {
+    rflx_clearCanvas()
+    rflx_ctx.fillStyle = "crimson";
+    rflx_ctx.beginPath();
+    rflx_ctx.arc(x, y, s, 0, 2 * Math.PI);
+    rflx_ctx.fill();
+
+    if (rflx_circleY + rflx_radius >= canvas.height) {
+        rflx_dY *= -rflx_energy
+        rflx_circleY = canvas.height - rflx_radius
+        rflx_dX *= rflx_friction
+    }
+    // top bound / ceiling
+    if (rflx_circleY - rflx_radius <= 0) {
+        rflx_dY *= -rflx_energy
+        rflx_circleY = rflx_radius
+        rflx_dX *= rflx_friction
+    }
+
+    // left bound
+    if (rflx_circleX - rflx_radius <= 0) {
+        rflx_dX *= -rflx_energy
+        rflx_circleX = rflx_radius
+    }
+    // right bound
+    if (rflx_circleX + rflx_radius >= canvas.width) {
+        rflx_dX *= -rflx_energy
+        rflx_circleX = canvas.width - rflx_radius
+    }
+
+    // reset insignificant amounts to 0
+    if (rflx_dX < 0.01 && rflx_dX > -0.01) {
+        rflx_dX = 0
+        rflx_circleX = canvas.width / 2
+        rflx_circleY = (Math.random() * 150) + 10;
+        rflx_dX = 2;
+        rflx_dY = -2;
+
+    }
+    if (rflx_dX < 0.01 && rflx_dX > -0.01) {
+        rflx_dX = 0
+    }
+
+    rflx_dY += rflx_gravity;
+
+    rflx_circleY += rflx_dY
+    rflx_circleX += rflx_dX
+}
+
+function rflx_clearCanvas() {
+    rflx_ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+rflx_drawCircle();
