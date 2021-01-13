779618cf6a2798c393350bd60f5b0dd15a9cc573
Jaemin Kim <jkim37@g.emporia.edu>
2021-01-13 16:44:40 -0600

    color

--- src/js/wavelength.js
+++ src/js/wavelength.js
@@ -1,28 +1,58 @@
 var canvas = document.getElementById('wavelength');
 var ctx_wavelength = canvas.getContext('2d');
 var canvasInterval = requestAnimationFrame(wavelength_canvasAnimation);
-var wavelength_circleX = 0;
-var wavelength_circleY = canvas.height / 2;
+var wavelength_circleX = canvas.width / 2;
+var wavelength_circleY = canvas.height / 2
+var wavelength_radius = 0
+var wavelength_flag = false
+var wavelength_palette = ['orange', 'green', 'yello', 'purple', 'black']
 
+
+function wavelength_colorPick(){
+    return Math.floor(Math.random()*10);
+}
 
 function wavelength_canvasAnimation() {
-    wavelength_drawCircle(canvas.width / 2, canvas.height / 2, 10);
+
+    if(!wavelength_flag){
+        wavelength_radius+=1;
+        if(wavelength_radius == 130){
+            wavelength_flag = true;
+        }
+    }
+    else if(wavelength_flag){
+        wavelength_radius -= 0.5;
+        if(wavelength_radius == 0){
+            wavelength_flag = false;
+        }
+    }
+
+
+    wavelength_drawCircle(wavelength_circleX, wavelength_circleY, wavelength_radius);
     canvasInterval = requestAnimationFrame(wavelength_canvasAnimation);
 }
 
 function wavelength_drawCircle(x, y, s) {
-    wavelength_clearCanvas()
-    ctx_wavelength.fillStyle = "crimson";
+    var i = wavelength_colorPick()
+    ctx_wavelength.fillStyle = wavelength_palette[i];
     ctx_wavelength.beginPath();
     ctx_wavelength.arc(x, y, s, 0, 2 * Math.PI);
     ctx_wavelength.fill();
-    ctx_wavelength.lineWidth = 5
+    ctx_wavelength.lineWidth = (Math.random() * 20) + 1
     ctx_wavelength.strokeStyle = "yellow"
     ctx_wavelength.stroke();
-    
-    if (wavelength_circleX > canvas.height * 1.1) {
-        wavelength_circleX = 0;
-    }
+    ctx_wavelength.lineWidth = (Math.random() * 20) + 1
+    ctx_wavelength.strokeStyle = "green"
+    ctx_wavelength.stroke();
+    ctx_wavelength.lineWidth = (Math.random() * 20) + 1
+    ctx_wavelength.strokeStyle = "purple"
+    ctx_wavelength.stroke();
+    ctx_wavelength.lineWidth = (Math.random() * 20) + 1
+    ctx_wavelength.strokeStyle = "black"
+    ctx_wavelength.stroke();
+
+
+
 }
 
 function wavelength_clearCanvas() {
